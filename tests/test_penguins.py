165a091f4641aa0080f5b11af1b607481839799f
felipepenha <felipe.penha@alumni.usp.br>
2021-03-17 18:00:19 -0400

    fix: reintroduced flake8 now consistent with black linting

--- tests/test_penguins.py
+++ tests/test_penguins.py
@@ -1,5 +1,7 @@
 import pandera as pa
-from src.main import data_sourcing
+from src.main import (
+    data_sourcing,
+)
 
 
 def test_import_data():
@@ -8,20 +10,45 @@ def test_import_data():
 
     print(df)
 
-    cats_sex = ["male", "female"]
-    cats_species = ["Adelie", "Gentoo", "Chinstrap"]
+    cats_sex = [
+        "male",
+        "female",
+    ]
+    cats_species = [
+        "Adelie",
+        "Gentoo",
+        "Chinstrap",
+    ]
 
     schema = pa.DataFrameSchema(
         {
-            "bill_length_mm": pa.Column(float, nullable=True),
-            "bill_depth_mm": pa.Column(float, nullable=True),
-            "flipper_length_mm": pa.Column(float, nullable=True),
-            "body_mass_g": pa.Column(float, nullable=True),
-            "sex": pa.Column(str, checks=pa.Check.isin(cats_sex), nullable=True),
+            "bill_length_mm": pa.Column(
+                float,
+                nullable=True,
+            ),
+            "bill_depth_mm": pa.Column(
+                float,
+                nullable=True,
+            ),
+            "flipper_length_mm": pa.Column(
+                float,
+                nullable=True,
+            ),
+            "body_mass_g": pa.Column(
+                float,
+                nullable=True,
+            ),
+            "sex": pa.Column(
+                str,
+                checks=pa.Check.isin(cats_sex),
+                nullable=True,
+            ),
             "species": pa.Column(
-                str, checks=pa.Check.isin(cats_species), nullable=True
+                str,
+                checks=pa.Check.isin(cats_species),
+                nullable=True,
             ),
         }
     )
 
-    validated_df = schema(df)
+    schema(df)
