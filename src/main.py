c55fb3a9337ca9f7e0e289b77c918111ff2cd371
felipepenha <felipe.penha@alumni.usp.br>
2021-04-22 18:05:20 -0400

    feat: working performance monitoring module

--- src/main.py
+++ src/main.py
@@ -13,7 +13,6 @@ import pandas as pd
 pd.set_option("display.max_rows", 100)
 pd.set_option("display.max_columns", None)
 pd.set_option("display.width", None)
-pd.set_option("display.max_colwidth", -1)
 
 
 @task
